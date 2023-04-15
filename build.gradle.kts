26471891ea92428975696db28fb079354d7eda23
Mani Shankar <shankarchowdary7545@gmail.com>
2023-04-15 20:51:13 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.4.0")
+        classpath("com.google.gms:google-services:4.3.15")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.5")
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.com.android.library) apply false
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Prototype Builder"
+include(":data")
+include(":presentation:admin")
+include(":presentation:prototype")
